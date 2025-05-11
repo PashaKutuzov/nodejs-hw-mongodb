a3312ddb6a2cfcbf146a8a4468eddf6a6a1cedf0
PashaKutuzov <pasha260605@gmail.com>
2025-05-11 14:25:06 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import  setupServer  from './server.js';
+console.log('Starting app...');
+(async () => {
+  try {
+    await setupServer();
+  } catch (error) {
+    console.error('Error while starting server:', error);
+  }
+})();
--- src/server.js
+++ src/server.js
@@ -1,3 +1,100 @@
-function setupServer(params) {
+import 'dotenv/config';
+import pino from 'pino-http';
+import cors from 'cors';
+import express from 'express';
+import initMongoConnection from "./db/initMongoConnection.js";
+import { contactModel } from './models/contacts.js';
+const app = express();
+
+export default async function setupServer() {
+
+console.log('setupServer is running...');
+
+ app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+    
+    
+
+  app.get('/api/contacts', async (req, res) => {
+  const contacts = await contactModel.find();
+  if (contacts.length === 0) {
+    res.status(404).json({ message: 'Not found' });
+  }
+
+    //   res.status(200).json({
+    //       status: 200,
+    //   message: 'Successfully found contacts!',
+    //   data: contacts,
+    //   });
+      
+      
+      
+      res.status(200).json({
+          message: 'Successfully found contacts!',
+          data: contacts
+      });
+  });
+
+
+   app.get('/api/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await contactModel.findById(contactId);
+ const allContacts = await contactModel.find();
+  console.log('All contacts in DB:', allContacts);
+    
+
+      
+
+        if (contact === null) {
+             res.status(404).json({
+            message: 'Not found'
+        });
+        };
+
+
+      res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+   });
+    
+    app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+    
+//serv
+    
+
+
+    try {
+    const PORT = process.env.PORT || 3000;
+   await initMongoConnection();
+    
+
+    app.listen(PORT, (error) => {
+        if (error) {
+            throw error;
+        }
+
+        console.log(`Server is running on port ${PORT}`);
+        
+    }); 
+} catch (error) {
+    console.log(error);
     
 }
+    //serv
+};
+
+
+
+// export default app;
